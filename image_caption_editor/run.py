75e5cd455a0078c90cd1041d65c2b64b22d09df0
Sijuade Oguntayo <cydalsij@outlook.com>
2024-05-21 23:36:45 +0100

    Update run.py

--- image_caption_editor/run.py
+++ image_caption_editor/run.py
@@ -1,7 +1,6 @@
 import subprocess
 
 def run():
-    """Run the Streamlit app using subprocess to invoke the CLI correctly."""
     subprocess.run(['streamlit', 'run', "image_caption_editor/app.py"], check=True)
 
 if __name__ == "__main__":
