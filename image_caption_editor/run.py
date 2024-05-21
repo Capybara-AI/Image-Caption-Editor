fb2f7d3989fbdaa1ed61fc4529eeff85bc78c7b2
Sijuade Oguntayo <cydalsij@outlook.com>
2024-05-21 23:36:32 +0100

    Update run.py

--- image_caption_editor/run.py
+++ image_caption_editor/run.py
@@ -2,7 +2,6 @@ import subprocess
 
 def run():
     """Run the Streamlit app using subprocess to invoke the CLI correctly."""
-    # Absolute path to your app.py or relative from the current working directory
     subprocess.run(['streamlit', 'run', "image_caption_editor/app.py"], check=True)
 
 if __name__ == "__main__":
